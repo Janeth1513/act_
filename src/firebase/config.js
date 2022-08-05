5bc9bbebc17c6cbd620c62ab06128d866071b6a6
Janeth1513 <94811417+Janeth1513@users.noreply.github.com>
2022-08-04 21:12:27 -0600

    funcionando

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8gNyMGk2XS8axBVJ7Koh9cDPmSA5PPAY",
-  authDomain: "actividad-15-1c02d.firebaseapp.com",
-  databaseURL: "https://actividad-15-1c02d-default-rtdb.firebaseio.com",
-  projectId: "actividad-15-1c02d",
-  storageBucket: "actividad-15-1c02d.appspot.com",
-  messagingSenderId: "949898823742",
-  appId: "1:949898823742:web:68163a577caf3db2dbe96f"
+  apiKey: "AIzaSyDbPI7KP2mKvPHCoCEcXUGzGDLuWL19f9U",
+  authDomain: "actividad-15-d439e.firebaseapp.com",
+  databaseURL: "https://actividad-15-d439e-default-rtdb.firebaseio.com",
+  projectId: "actividad-15-d439e",
+  storageBucket: "actividad-15-d439e.appspot.com",
+  messagingSenderId: "936783883259",
+  appId: "1:936783883259:web:f48d05d86edcd5f5a3395e"
 };
 
 const db = initializeApp(firebaseConfig);
